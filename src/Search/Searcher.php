7bf029e037fe75a6ac10daf57465683ac3d9b12a
Olaoluwa Olorunda <olorundaolaoluwa@gmail.com>
2022-05-16 16:37:35 +0100

    First Commit

---
+++ src/Search/Searcher.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace MercuryHolidays\Search;
+
+class Searcher
+{
+    private $properties;
+    /**
+     * @var int
+     */
+    private $rooms_required;
+    /**
+     * @var array
+     */
+    private array $available_and_within_budget_grouped_by_hotel_and_floor=[];
+    private array $available_rooms=[];
+    /**
+     * @var array
+     */
+    private array $available_and_within_budget=[];
+
+
+    public function add(array $property) : Searcher
+    {
+        $this->properties= $property;
+        return $this;
+    }
+
+    public function search(int $roomsRequired, $minimum, $maximum): Searcher
+    {
+        $this->rooms_required=$roomsRequired;
+        $room_count=0;
+        foreach($this->properties as $property){
+            if($property['available']=='True' && ($property['per_room_price'] >=$minimum && $property['per_room_price']<=$maximum)){
+                   $this->available_and_within_budget_grouped_by_hotel_and_floor[$property['name']][$property['floor']][]=$property;
+                   $this->available_and_within_budget[]=$property;
+                   $room_count++;
+            }
+        }
+
+        return $this;
+    }
+
+
+
+    public function ensureRoomBelongToThesameHotelAndRoomIsAdjacentAndOnThesameFloor() : array
+    {
+        if($this->rooms_required==1){
+            return $this->available_and_within_budget;
+        }
+        $count=0;
+        foreach($this->available_and_within_budget_grouped_by_hotel_and_floor as $available_and_within_budget_hotel){
+
+            foreach($available_and_within_budget_hotel as $available_and_within_budget_floor){
+
+                if(count($available_and_within_budget_floor)>=$this->rooms_required){
+
+                    foreach($available_and_within_budget_floor as $withing_thesame_floor){
+
+                        if($count==$this->rooms_required){
+                            return $this->available_rooms;
+                        }
+
+                        $this->available_rooms[]=$withing_thesame_floor;
+                        $this->removeRoomNotAdjacent($available_and_within_budget_floor,$count);
+                        $count ++;
+                    }
+                }
+            }
+        }
+        return count(array_values($this->available_rooms))<$this->rooms_required ? [] : array_values($this->available_rooms);
+    }
+
+
+    private function removeRoomNotAdjacent($available_and_within_budget,&$count) : void
+    {
+        if(isset($available_and_within_budget[$count+1]['room_no']) && isset($this->available_rooms[$count]['room_no'])){
+            if(abs($this->available_rooms[$count]['room_no']-$available_and_within_budget[$count+1]['room_no'])>1){
+                unset($this->available_rooms[$count]);
+                $count --;
+            }
+        }
+    }
+}
+
+//
+
+
+
---
+++ tests/Search/SearcherTest.php
@@ -0,0 +1,75 @@
+<?php
+
+
+use MercuryHolidays\Search\Searcher;
+use PHPUnit\Framework\TestCase;
+
+class SearcherTest extends TestCase
+{
+
+    private $hotel_availability_list = [
+        ['name' => 'Hotel A', 'available' => 'False', 'floor' => 1, 'room_no' => 1, 'per_room_price' => 25.80],
+        ['name' => 'Hotel A', 'available' => 'False', 'floor' => 1, 'room_no' => 2, 'per_room_price' => 25.80],
+        ['name' => 'Hotel A', 'available' => 'True', 'floor' => 1, 'room_no' => 3, 'per_room_price' => 25.80],
+        ['name' => 'Hotel A', 'available' => 'True', 'floor' => 1, 'room_no' => 4, 'per_room_price' => 25.80],
+        ['name' => 'Hotel A', 'available' => 'False', 'floor' => 1, 'room_no' => 5, 'per_room_price' => 25.80],
+        ['name' => 'Hotel A', 'available' => 'False', 'floor' => 2, 'room_no' => 6, 'per_room_price' => 30.10],
+        ['name' => 'Hotel A', 'available' => 'True', 'floor' => 2, 'room_no' => 7, 'per_room_price' => 35.00],
+        ['name' => 'Hotel B', 'available' => 'True', 'floor' => 1, 'room_no' => 1, 'per_room_price' => 45.80],
+        ['name' => 'Hotel B', 'available' => 'False', 'floor' => 1, 'room_no' => 2, 'per_room_price' => 45.80],
+        ['name' => 'Hotel B', 'available' => 'True', 'floor' => 1, 'room_no' => 3, 'per_room_price' => 45.80],
+        ['name' => 'Hotel B', 'available' => 'True', 'floor' => 1, 'room_no' => 4, 'per_room_price' => 45.80],
+        ['name' => 'Hotel B', 'available' => 'False', 'floor' => 1, 'room_no' => 5, 'per_room_price' => 45.80],
+        ['name' => 'Hotel B', 'available' => 'False', 'floor' => 2, 'room_no' => 6, 'per_room_price' => 49.00],
+        ['name' => 'Hotel B', 'available' => 'False', 'floor' => 2, 'room_no' => 7, 'per_room_price' => 49.00]
+    ];
+
+
+    public function testSearchReturnsCorrectResultMinimum20Maximum30RoomRequired2(): void
+    {
+
+        $searcher = (new Searcher())
+                        ->add($this->hotel_availability_list)
+                        ->search(2, 20, 30)
+                        ->ensureRoomBelongToThesameHotelAndRoomIsAdjacentAndOnThesameFloor();
+//die(print_r($searcher));
+        $this->assertEquals($searcher,
+            [
+                ["name" => "Hotel A", "available" => "True", "floor" => 1, "room_no" => 3, "per_room_price" => 25.80],
+                ["name" => "Hotel A", "available" => "True", "floor" => 1, "room_no" => 4, "per_room_price" => 25.80]
+            ]
+        );
+
+    }
+
+    public function testSearchReturnsCorrectResultMinimum30Maximu50RoomRequired2(): void
+    {
+        $searcher = (new Searcher())
+                        ->add($this->hotel_availability_list)
+                        ->search(2, 30, 50)
+                        ->ensureRoomBelongToThesameHotelAndRoomIsAdjacentAndOnThesameFloor();
+
+        $this->assertEqualsCanonicalizing($searcher,
+            [
+                ["name" => "Hotel B", "available" => "True", "floor" => 1, "room_no" => 3, "per_room_price" => 45.80],
+                ["name" => "Hotel B", "available" => "True", "floor" => 1, "room_no" => 4, "per_room_price" => 45.80]
+            ]
+        );
+    }
+
+    public function testSearchReturnsCorrectResultMinimum25Maximu40RoomRequired1(): void
+    {
+        $searcher = (new Searcher())
+                         ->add($this->hotel_availability_list)->search(1, 25, 40)
+                         ->ensureRoomBelongToThesameHotelAndRoomIsAdjacentAndOnThesameFloor();
+
+        $this->assertEqualsCanonicalizing($searcher,
+            [
+                ["name" => "Hotel A", "available" => "True", "floor" => 1, "room_no" => 3, "per_room_price" => 25.80],
+                ["name" => "Hotel A", "available" => "True", "floor" => 1, "room_no" => 4, "per_room_price" => 25.80],
+                ["name" => "Hotel A", "available" => "True", "floor" => 2, "room_no" => 7, "per_room_price" => 35.00]
+            ]
+        );
+    }
+}
+
